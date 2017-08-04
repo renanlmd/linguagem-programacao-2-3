8f37e5f05f843ff0d8420d556652665bb69858cf
RodrigoBRamos <systemas.rodrigo@gmail.com>
2017-08-04 20:11:39 -0300

    Pode melhorar visualmente <BR>
    colocando um <br>, visualmente melhorar, bom pra estruturar resultados

--- aula-1/for/alexcleiton.php
+++ aula-1/for/alexcleiton.php
@@ -2,6 +2,6 @@
 	echo "Primeiramente.... Olá mundo!\n";
 	echo "Segundamente.... Bolsonaro 2018!\n\n"; 
 	for ($i=0; $i < 10 ; $i++) { 
-		echo "'$i' tentativas\n";
+		echo "<br>'$i' tentativas\n";
 	}
 ?>
