bb33663e62d289596d18292ca039e156c4a9964a
renan <renan.webtec@hotmail.com>
2017-08-05 21:43:54 -0300

    Adicionando meu primeiro conteudo ao repositório

---
+++ aula-1/for/renan.php
@@ -0,0 +1,17 @@
+<?php 
+
+$num1 = 2;
+$num2 = 5;
+echo "Soma:<br>";
+for($i=1; $i<=10; $i++){
+	
+	$soma = $num1 + $i;
+	echo "$num1 + $i = $soma<br>";	
+	
+	
+}
+echo "<br>Multiplicação:<br>";
+for($i=1;$i<=10; $i++){
+	$mult = $num1 * $i;
+	echo "$num1 x $i = $mult<br>";
+}
