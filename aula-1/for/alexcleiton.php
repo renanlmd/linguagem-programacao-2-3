17bd32cbdc166aa72326e80e1b5c348d9647db11
Alex Rodrigues <alexcleiton16@gmail.com>
2017-08-06 03:39:30 -0300

    alterações no codigo e adição de tags html

--- aula-1/for/alexcleiton.php
+++ aula-1/for/alexcleiton.php
@@ -1,6 +1,6 @@
 <?php
-	echo "Primeiramente.... Olá mundo!\n";
-	echo "Segundamente.... Bolsonaro 2018!\n\n"; 
+	echo "Primeiramente.... Olá mundo!<br>";
+	echo "Segundamente.... Bolsonaro 2018!<br><br>"; 
 	for ($i=0; $i < 10 ; $i++) { 
 		echo "<br>'$i' tentativas\n";
 	}
