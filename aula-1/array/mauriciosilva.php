fd5865880048e5beeb0987d2408c0731dd2ed10c
Mauricio <mausilva828@gmail.com>
2017-08-07 17:53:28 -0300

    1 commit

--- aula-1/array/mauriciosilva.php
+++ aula-1/array/mauriciosilva.php
@@ -3,13 +3,13 @@
 /**
  * Um array no PHP é atualmente um mapa ordenado. Um mapa é um tipo que relaciona *
  * valores para chaves. Este tipo é otimizado de várias maneiras, então você pode *
- * usá-lo como um array real, ou uma lista (vetor), hashtable (que é uma implementação 
- * de mapa), dicionário, coleção, pilha, fila e provavelmente mais. Como você pode ter 
+ * usá-lo como um array real, ou uma lista (vetor), hashtable (que é uma implementação
+ * de mapa), dicionário, coleção, pilha, fila e provavelmente mais. Como você pode ter
  * outro array PHP como um valor, você pode facilmente simular árvores.
  *
  * fonte: http://php.net/manual/pt_BR/language.types.array.php
  */
-
+echo "paulo moura";
 $array = [
   "a", "b", "c", "d"
 ];
