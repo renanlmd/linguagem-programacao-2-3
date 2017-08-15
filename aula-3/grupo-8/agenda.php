3c3e3d0e9b8b1cc5f6fce302a3299750d2b034b5
joaovc <joaovcsantosap@gmail.com>
2017-08-15 19:00:20 -0300

    Tentando fazergit status, desculpem a demora só pude ver na segunda feira e commitar hoje

--- aula-3/grupo-8/agenda.php
+++ aula-3/grupo-8/agenda.php
@@ -1,17 +1,35 @@
 <?php
-
-	
-
 	class Agenda{
-		
-		protected $nome;
-		protected $email;
 
-		public function novo_contato{
-			return $this->nome = $nome;
-			return $this->email= $email;
+		protected $contato = [];
+
+		// ADICIONANDO UM CONTATO À MATRIZ AGENDA
+		public function novoContato($n, $e){
+				$this->contato[] = [$n,$e];
 		}
-	}
+
+		// BUSCANDO UM CONTATO PELO NOME
+		public function buscaContato($n){
+			foreach ($this->contato as $k => $v) {
+				if (strtoupper($n) == strtoupper($v[0])){
+						$r = "\nINDICE: [$k] | NOME: [".$v[0]."] | EMAIL: [".$v[1]."]\n";
+						return $r;
+				}
+			}
+		}
+
+		// APAGANDO UM CONTATO PELO INDICE DO ARRAY PRINCIPAL
+		public function apagaContato($k){
+			if (isset($k)) {
+				unset($this->contato[$k]);
+			}else {
+				echo "\nFaça a busca para poder apagar um contato!\n";
+			}
+		}
+
+		// EXIBE A AGENDA
+		public function getContato(){
+			return $this->contato;
+		}
 
 }
-?>
--- aula-3/grupo-8/index.php
+++ aula-3/grupo-8/index.php
@@ -1,12 +1,34 @@
-<?php 
+<?php
 
-	require __DIR__ . '/contato.php';
-	require __DIR__ . '/agenda.php';
+	include("contato.php");
 
-	$agenda = new Agenda ('Nome','email');
-	$contato = new Contato ('Erison','erisonsantos111@gmail.com');
+	$agenda = new Agenda();
 
-	echo "{$contato->GetNome()} - {$email->GetEmail()}";
-	
+	$contato1 = new Contato("João Victor","joaovcsantosap@gmail.com");
+	$contato2 = new Contato("Erison","erisonsantos@gmail.com");
+	$contato3 = new Contato("Joas","joasribeiro@gmail.com");
 
-?>
+	$n1 = $contato1->getNome();
+	$e1 = $contato1->getEmail();
+
+	$n2 = $contato2->getNome();
+	$e2 = $contato2->getEmail();
+
+	$n3 = $contato3->getNome();
+	$e3 = $contato3->getEmail();
+
+	$agenda->novoContato($n1, $e1);
+	$agenda->novoContato($n2, $e2);
+	$agenda->novoContato($n3, $e3);
+
+	$busca = $agenda->buscaContato("erison");
+
+	var_dump($agenda);
+	echo "<hr>";
+	var_dump($contato1);
+	echo "<hr>";
+	var_dump($contato2);
+	echo "<hr>";
+	var_dump($contato3);
+
+	echo $busca;
