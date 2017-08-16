4027eff8c3c5f7c54813d4416898f8ba2dd2f43e
ErisonSantos <erisonsantos111@gmail.com>
2017-08-16 20:51:39 -0300

    Atividade grupo 8:muscle:

--- aula-3/grupo-8/agenda.php
+++ aula-3/grupo-8/agenda.php
@@ -1,35 +1,43 @@
 <?php
+
 	class Agenda{
 
-		protected $contato = [];
+		protected $contatos = [];
 
 		// ADICIONANDO UM CONTATO À MATRIZ AGENDA
-		public function novoContato($n, $e){
-				$this->contato[] = [$n,$e];
+		public function novocontato($n, $e){
+				$this->contatos[] = [$n,$e];
 		}
 
 		// BUSCANDO UM CONTATO PELO NOME
-		public function buscaContato($n){
-			foreach ($this->contato as $k => $v) {
+		public function buscacontato($n){
+			foreach ($this->contatos as $k => $v) {
 				if (strtoupper($n) == strtoupper($v[0])){
-						$r = "\nINDICE: [$k] | NOME: [".$v[0]."] | EMAIL: [".$v[1]."]\n";
-						return $r;
+						#$r = "\nINDICE: [$k] | NOME: [".$v[0]."] | EMAIL: [".$v[1]."]\n";
+						return $this->contato[$k] = [$v[0],$v[1]];
 				}
+			}
+			if ($n != $v[0]) {
+				return $r = "Contato não existe!\n";
 			}
 		}
 
 		// APAGANDO UM CONTATO PELO INDICE DO ARRAY PRINCIPAL
-		public function apagaContato($k){
-			if (isset($k)) {
-				unset($this->contato[$k]);
+		public function apagacontato($n){
+			if (isset($n)) {
+				foreach ($this->contatos as $k => $v) {
+					if ($n == $v[0]) {
+						unset($this->contatos[$k]);
+					}
+				}
 			}else {
-				echo "\nFaça a busca para poder apagar um contato!\n";
+				echo "\nFaça a busca para poder apagar um contatos!\n";
 			}
 		}
 
 		// EXIBE A AGENDA
-		public function getContato(){
-			return $this->contato;
+		public function getcontato(){
+			return $this->contatos;
 		}
 
 }
--- aula-3/grupo-8/index.php
+++ aula-3/grupo-8/index.php
@@ -4,8 +4,8 @@
 
 	$agenda = new Agenda();
 
-	$contato1 = new Contato("João Victor","joaovcsantosap@gmail.com");
-	$contato2 = new Contato("Erison","erisonsantos@gmail.com");
+	$contato1 = new Contato("João","joaovcsantosap@gmail.com");
+	$contato2 = new Contato("Erison","erisonsantos111@gmail.com");
 	$contato3 = new Contato("Joas","joasribeiro@gmail.com");
 
 	$n1 = $contato1->getNome();
@@ -21,14 +21,26 @@
 	$agenda->novoContato($n2, $e2);
 	$agenda->novoContato($n3, $e3);
 
-	$busca = $agenda->buscaContato("erison");
+	$busca = $agenda->buscaContato("fre");
+
+	#$apaga = $agenda->apagaContato($busca);
 
 	var_dump($agenda);
-	echo "<hr>";
+	echo "____________________________________________________";
 	var_dump($contato1);
-	echo "<hr>";
+	echo "____________________________________________________";
 	var_dump($contato2);
-	echo "<hr>";
+	echo "____________________________________________________";
 	var_dump($contato3);
 
-	echo $busca;
+	if (@$busca) {
+		if (is_array($busca)) {
+			echo "\n # RESULTATDO DA BUSCA # \n";
+			echo "\nNome: [".$busca[0]."]\n";
+			echo "\nEmail: [".$busca[1]."]\n";
+		}else {
+			echo $busca;
+		}
+	}elseif (@$apaga) {
+
+	}
