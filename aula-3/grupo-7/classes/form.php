48b3f8951d2e9b4a1aec5cd3c25b4b4c1c5510bf
root <root@localhost.localdomain>
2017-08-17 10:23:06 -0300

    Grupo 7

---
+++ aula-3/grupo-7/classes/form.php
@@ -0,0 +1,55 @@
+<?php 
+
+class Form
+{
+	
+	private $resultado;
+	public function calcularArea(){
+
+
+	}
+
+}
+
+class Retangulo extends Form
+{
+
+ 	private $largura;
+ 	private $altura;
+ 	
+ 	public function __construct($altura,$largura){
+ 		$this->largura=$largura;
+ 		$this->altura=$altura;
+ }
+ 	public function calcularArea(){
+
+		$this->resultado = ($this->largura) * ($this->altura);
+		echo $this->resultado;
+ 	}
+}
+class Quadrado	 extends Retangulo{
+ 	public function __construct($lado){
+ 		$this->largura=$lado;
+ 		$this->altura=$lado;
+ }
+	public function calcularArea(){
+
+		$this->resultado = ($this->largura) * ($this->altura);
+		echo $this->resultado;
+
+	} 
+
+}
+
+class Circulo extends Form
+{
+ 	private $raio;
+ 	public function __construct($raio){
+ 		$this->raio=$raio;
+
+ }
+ 	public function calcularArea(){
+		$this->resultado= pow($this->raio, 2) * pi();
+		echo $this->resultado;
+ }
+}
---
+++ aula-3/grupo-7/main.php
@@ -0,0 +1,14 @@
+<?php include("classes/form.php");
+
+$areaQ = new Retangulo(10,20);
+
+$areaR = new Quadrado(30);
+
+$areaC = new Circulo(50);
+
+$result=$areaQ->calcularArea();
+echo "\n";
+$result=$areaR->calcularArea();
+echo "\n";
+$result=$areaC->calcularArea();
+echo "\n";
