4490b65f4daabae1beae9ce6f90ec3bc5b34bb81
root <root@localhost.localdomain>
2017-08-17 10:32:38 -0300

    Grupo 7

--- aula-3/grupo-7/index.php
+++ aula-3/grupo-7/index.php
@@ -1,9 +1,7 @@
 <?php include("classes/form.php");
 
 $areaQ = new Retangulo(10,20);
-
 $areaR = new Quadrado(30);
-
 $areaC = new Circulo(50);
 
 $result=$areaQ->calcularArea();
@@ -11,4 +9,4 @@ echo "\n";
 $result=$areaR->calcularArea();
 echo "\n";
 $result=$areaC->calcularArea();
-echo "\n";
+echo "\n"
