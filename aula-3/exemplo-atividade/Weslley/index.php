e45fe9073da4a904002f69c7c87d3a19003bdab0
weslley <weslleythiago96@gmail.com>
2017-08-24 15:45:19 -0300

    pasta para atividade

---
+++ aula-3/exemplo-atividade/Weslley/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style type="text/css">
+      .col{
+        border: 1px solid;
+      }
+      .topo{
+        background: red;
+        height: 200px;
+      }
+    </style>
+  </head>
+  <body>
+   <div class="container">
+      <div class="row topo" >
+        <div class="col-8">
+          1 of 2
+        </div>
+        <div class="col">
+          2 of 2
+        </div>
+      </div>
+    
+      <div class="row">
+        <div class="col">
+          1 of 3
+        </div>
+        <div class="col">
+          2 of 3
+        </div>
+        <div class="col">
+          3 of 3
+        </div>
+      </div>
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.slim.min.js" ></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
