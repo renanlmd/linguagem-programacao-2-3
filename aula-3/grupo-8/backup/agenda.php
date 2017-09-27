708e3d28bc24f5fd2267714b6f84ea458d784f0c
joaovc <joaovcsantosap@gmail.com>
2017-09-27 18:21:42 -0300

    Update atividade aula-3

---
+++ aula-3/grupo-8/backup/agenda.php
@@ -0,0 +1,46 @@
+<?php
+
+	class Agenda{
+
+		protected $contatos = [];
+
+		// ADICIONANDO UM CONTATO À MATRIZ AGENDA
+		public function novoContato($n,$e){
+				$this->contatos[] = [$n,$e];
+		}
+
+		// BUSCANDO UM CONTATO PELO NOME
+		public function buscaContato($n){
+			foreach ($this->contatos as $k => $v) {
+				if (strtoupper($n) == strtoupper($v[0])){
+						#$r = "\nINDICE: [$k] | NOME: [".$v[0]."] | EMAIL: [".$v[1]."]\n";
+						return $this->contato[$k] = $v[0];
+				}
+			}
+			if ($n != $v[0]) {
+				return $r = "Contato não existe!\n";
+			}
+		}
+
+		// APAGANDO UM CONTATO PELO INDICE DO ARRAY PRINCIPAL
+		public function apagaContato($n){
+			$nome = strtoupper($n);
+			if (isset($nome)) {
+				foreach ($this->contatos as $k => $v) {
+					if ($nome == strtoupper($v[0])) {
+						@$en = strtoupper($v[0]);
+						unset($this->contatos[$k]);
+					}
+				}
+				if ($nome != @$en) {
+					return $r = "Você não pode apagar um contato que não existe!\n";
+				}
+			}
+		}
+
+		// EXIBE A AGENDA
+		public function getcontato(){
+			return $this->contatos;
+		}
+
+}
